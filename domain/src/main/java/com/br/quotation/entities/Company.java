6da85e5bdeabd3662633fe8ae3d44dc1bb6d1511
Robsonndumbledore <robsonndumbledore@gmail.com>
2021-01-31 15:23:49 -0300

    feat: create abstract company class

---
+++ domain/src/main/java/com/br/quotation/entities/Company.java
@@ -0,0 +1,37 @@
+package com.br.quotation.entities;
+
+/**
+ *
+ * @author Robson
+ */
+public abstract class Company {
+    
+    private Long id;
+    private String name;
+    private String cnpj;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
+}
