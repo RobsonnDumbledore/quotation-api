92846696936467ef1028f05c719917b58c3492db
Robsonndumbledore <robsonndumbledore@gmail.com>
2021-01-31 15:33:35 -0300

    feat: create (Merchant) entity

---
+++ domain/src/main/java/com/br/quotation/entities/Merchant.java
@@ -0,0 +1,16 @@
+package com.br.quotation.entities;
+
+/**
+ *
+ * @author Robson
+ */
+public class Merchant extends Company{
+
+    public Merchant() {
+    }
+
+    public Merchant(Long id, String name, String cnpj) {
+        super(id, name, cnpj);
+    }
+    
+}
