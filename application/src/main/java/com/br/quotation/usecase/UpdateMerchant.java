1583f5119c2c2e595fc13067856625da3647ac4c
Robsonndumbledore <robsonndumbledore@gmail.com>
2021-01-31 23:39:04 -0300

    feat: implement use case update merchant

---
+++ application/src/main/java/com/br/quotation/usecase/UpdateMerchant.java
@@ -0,0 +1,12 @@
+package com.br.quotation.usecase;
+
+import com.br.quotation.entities.Merchant;
+
+/**
+ *
+ * @author Robson
+ */
+public interface UpdateMerchant {
+
+    Merchant execute(Merchant merchant);
+}
---
+++ application/src/main/java/com/br/quotation/usecase/impl/UpdateMerchantImpl.java
@@ -0,0 +1,28 @@
+package com.br.quotation.usecase.impl;
+
+import javax.inject.Named;
+import javax.inject.Inject;
+import com.br.quotation.entities.Merchant;
+import com.br.quotation.usecase.UpdateMerchant;
+import com.br.quotation.repositories.MerchantRepository;
+
+/**
+ *
+ * @author Robson
+ */
+@Named
+public class UpdateMerchantImpl implements UpdateMerchant {
+
+    private MerchantRepository repository;
+
+    @Inject
+    public UpdateMerchantImpl(MerchantRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Merchant execute(Merchant merchant) {
+        return repository.update(merchant);
+    }
+
+}
