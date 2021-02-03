400c83ed9e05f669214ab630083825970490798f
Robsonndumbledore <robsonndumbledore@gmail.com>
2021-02-02 21:53:34 -0300

    feat: implement use case find provider

---
+++ application/src/main/java/com/br/quotation/usecase/provider/FindProvider.java
@@ -0,0 +1,14 @@
+package com.br.quotation.usecase.provider;
+
+import java.util.Optional;
+import com.br.quotation.entities.Provider;
+
+/**
+ *
+ * @author Robson
+ */
+public interface FindProvider {
+
+    Optional<Provider> execute(Provider provider);
+
+}
---
+++ application/src/main/java/com/br/quotation/usecase/provider/impl/FindProviderImpl.java
@@ -0,0 +1,29 @@
+package com.br.quotation.usecase.provider.impl;
+
+import javax.inject.Named;
+import java.util.Optional;
+import javax.inject.Inject;
+import com.br.quotation.entities.Provider;
+import com.br.quotation.usecase.provider.FindProvider;
+import com.br.quotation.repositories.ProviderRepository;
+
+/**
+ *
+ * @author Robson
+ */
+@Named
+public class FindProviderImpl implements FindProvider {
+
+    private ProviderRepository repository;
+
+    @Inject
+    public FindProviderImpl(ProviderRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Optional<Provider> execute(Provider provider) {
+        return repository.find(provider.getId());
+    }
+
+}
