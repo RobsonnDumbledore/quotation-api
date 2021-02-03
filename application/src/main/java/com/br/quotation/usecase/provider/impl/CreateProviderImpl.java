6c06a6f71498c411950977ab6d2c2a64328817b0
Robsonndumbledore <robsonndumbledore@gmail.com>
2021-02-02 21:44:28 -0300

    fix: add annotations to create supplier

--- application/src/main/java/com/br/quotation/usecase/provider/impl/CreateProviderImpl.java
+++ application/src/main/java/com/br/quotation/usecase/provider/impl/CreateProviderImpl.java
@@ -1,5 +1,7 @@
 package com.br.quotation.usecase.provider.impl;
 
+import javax.inject.Named;
+import javax.inject.Inject;
 import com.br.quotation.entities.Provider;
 import com.br.quotation.repositories.ProviderRepository;
 import com.br.quotation.usecase.provider.CreateProvider;
@@ -8,9 +10,15 @@ import com.br.quotation.usecase.provider.CreateProvider;
  *
  * @author Robson
  */
+@Named
 public class CreateProviderImpl implements CreateProvider {
 
     private ProviderRepository repository;
+
+    @Inject
+    public CreateProviderImpl(ProviderRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public Provider execute(Provider provider) {
